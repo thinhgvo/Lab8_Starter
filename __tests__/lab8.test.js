a04a1c32054833ab11bd95230c02fb9a3daf871a
Thinh Vo <75603309+thinhgvo@users.noreply.github.com>
2023-05-28 03:53:48 -0700

    All tests passed

--- __tests__/lab8.test.js
+++ __tests__/lab8.test.js
@@ -1,7 +1,7 @@
 describe('Basic user flow for Website', () => {
   // First, visit the lab 8 website
   beforeAll(async () => {
-    await page.goto('https://cse110-f2021.github.io/Lab8_Website');
+    await page.goto('http://127.0.0.1:5500/index.html');
   });
 
   // Next, check to make sure that all 20 <product-item> elements have loaded
@@ -23,15 +23,18 @@ describe('Basic user flow for Website', () => {
     let data, plainValue;
     // Query select all of the <product-item> elements
     const prodItems = await page.$$('product-item');
-    console.log(`Checking product item 1/${prodItems.length}`);
     // Grab the .data property of <product-items> to grab all of the json data stored inside
-    data = await prodItems[0].getProperty('data');
-    // Convert that property to JSON
-    plainValue = await data.jsonValue();
-    // Make sure the title, price, and image are populated in the JSON
-    if (plainValue.title.length == 0) { allArePopulated = false; }
-    if (plainValue.price.length == 0) { allArePopulated = false; }
-    if (plainValue.image.length == 0) { allArePopulated = false; }
+    const arrlength = prodItems.length;
+    for(let i = 0; i < arrlength; i++) {
+      console.log(`Checking product item ${i + 1}/${prodItems.length}`);
+      data = await prodItems[i].getProperty('data');
+      // Convert that property to JSON
+      plainValue = await data.jsonValue();
+      // Make sure the title, price, and image are populated in the JSON
+      if (plainValue.title.length == 0) { allArePopulated = false; }
+      if (plainValue.price.length == 0) { allArePopulated = false; }
+      if (plainValue.image.length == 0) { allArePopulated = false; }
+    }
     // Expect allArePopulated to still be true
     expect(allArePopulated).toBe(true);
 
@@ -50,6 +53,14 @@ describe('Basic user flow for Website', () => {
     // Grab the shadowRoot of that element (it's a property), then query a button from that shadowRoot.
     // Once you have the button, you can click it and check the innerText property of the button.
     // Once you have the innerText property, use innerText.jsonValue() to get the text value of it
+    const prodItem = await page.$('product-item');
+    let data = await prodItem.getProperty('shadowRoot');
+    let plainValue = await data.$('button');
+    await plainValue.click();
+    let innerText = await plainValue.getProperty('innerText');
+    let text = await innerText.jsonValue();
+    expect(text).toBe("Remove from Cart");
+    await plainValue.click();
   }, 2500);
 
   // Check to make sure that after clicking "Add to Cart" on every <product-item> that the Cart
@@ -60,6 +71,21 @@ describe('Basic user flow for Website', () => {
     // Query select all of the <product-item> elements, then for every single product element
     // get the shadowRoot and query select the button inside, and click on it.
     // Check to see if the innerText of #cart-count is 20
+    let data, plainValue;
+    const prodItems = await page.$$('product-item');
+    const arrlength = prodItems.length;
+    for(let i = 0; i < arrlength; i++) {
+      data = await prodItems[i].getProperty('shadowRoot');
+      plainValue = await data.$('button');
+      await plainValue.click();
+      let innerText = await plainValue.getProperty('innerText');
+      let text = await innerText.jsonValue();
+      expect(text).toBe("Remove from Cart");
+    }
+    let cart = await page.$('#cart-count');
+    let cartCount = await cart.getProperty('innerText');
+    let result = await cartCount.jsonValue();
+    expect(Number(result)).toBe(20);
   }, 10000);
 
   // Check to make sure that after you reload the page it remembers all of the items in your cart
@@ -69,6 +95,21 @@ describe('Basic user flow for Website', () => {
     // Reload the page, then select all of the <product-item> elements, and check every
     // element to make sure that all of their buttons say "Remove from Cart".
     // Also check to make sure that #cart-count is still 20
+    await page.reload();
+    let innerText, data, plainValue, text;
+    const prodItems = await page.$$('product-item');
+    const arrlength = prodItems.length;
+    for(let i = 0; i < arrlength; i++) {
+      data = await prodItems[i].getProperty('shadowRoot');
+      plainValue = await data.$('button');
+      innerText = await plainValue.getProperty('innerText');
+      text = await innerText.jsonValue();
+      expect(text).toBe("Remove from Cart");
+    }
+    let cart = await page.$('#cart-count');
+    let cartCount = await cart.getProperty('innerText');
+    let result = await cartCount.jsonValue();
+    expect(Number(result)).toBe(20);
   }, 10000);
 
   // Check to make sure that the cart in localStorage is what you expect
@@ -76,6 +117,10 @@ describe('Basic user flow for Website', () => {
     // TODO - Step 5
     // At this point he item 'cart' in localStorage should be 
     // '[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]', check to make sure it is
+    let cart = await page.evaluate(() => {
+      return localStorage.getItem('cart');
+    });
+    expect(cart).toBe('[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]');
   });
 
   // Checking to make sure that if you remove all of the items from the cart that the cart
@@ -85,6 +130,19 @@ describe('Basic user flow for Website', () => {
     // TODO - Step 6
     // Go through and click "Remove from Cart" on every single <product-item>, just like above.
     // Once you have, check to make sure that #cart-count is now 0
+    const prodItems = await page.$$('product-item');
+    const arrlength = prodItems.length;
+    for(let i = 0; i < arrlength; i++) {
+      let data = await prodItems[i].getProperty('shadowRoot');
+      let plainValue = await data.$('button');
+      await plainValue.click();
+    }
+    // let cart = await page.$('#cart-count');
+    // let cartCount = await cart.getProperty('innerText');
+    // let result = await cartCount.jsonValue();
+    // expect(Number(result)).toBe(0);
+    let cartCount = await page.$eval('#cart-count', (element) => element.innerHTML);
+    expect(Number(cartCount)).toBe(0);
   }, 10000);
 
   // Checking to make sure that it remembers us removing everything from the cart
@@ -95,6 +153,21 @@ describe('Basic user flow for Website', () => {
     // Reload the page once more, then go through each <product-item> to make sure that it has remembered nothing
     // is in the cart - do this by checking the text on the buttons so that they should say "Add to Cart".
     // Also check to make sure that #cart-count is still 0
+    await page.reload();
+    let innerText, data, plainValue, text;
+    const prodItems = await page.$$('product-item');
+    const arrlength = prodItems.length;
+    for(let i = 0; i < arrlength; i++) {
+      data = await prodItems[i].getProperty('shadowRoot');
+      plainValue = await data.$('button');
+      innerText = await plainValue.getProperty('innerText');
+      text = await innerText.jsonValue();
+      expect(text).toBe("Add to Cart");
+    }
+    let cart = await page.$('#cart-count');
+    let cartCount = await cart.getProperty('innerText');
+    let result = await cartCount.jsonValue();
+    expect(Number(result)).toBe(0);
   }, 10000);
 
   // Checking to make sure that localStorage for the cart is as we'd expect for the
@@ -103,5 +176,9 @@ describe('Basic user flow for Website', () => {
     console.log('Checking the localStorage...');
     // TODO - Step 8
     // At this point he item 'cart' in localStorage should be '[]', check to make sure it is
+    let cart = await page.evaluate(() => {
+      return localStorage.getItem('cart');
+    });
+    expect(cart).toBe('[]');
   });
 });
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,88 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+// Phone Number Tests
+test('Is phone number valid: true', () => {
+    expect(functions.isPhoneNumber('123-456-7890')).toBe(true);
+});
+
+test('Is phone number valid: true', () => {
+    expect(functions.isPhoneNumber('(123) 456-7890')).toBe(true);
+});
+
+test('Is phone number valid: false', () => {
+    expect(functions.isPhoneNumber('!@#$%^&*(')).toBe(false);
+});
+
+test('Is phone number valid: false', () => {
+    expect(functions.isPhoneNumber('abcdefghij')).toBe(false);
+});
+
+// Email Tests
+test('Is the email valid: true', () => {
+    expect(functions.isEmail('johndoe@gmail.com')).toBe(true);
+});
+
+test('Is the email valid: false', () => {
+    expect(functions.isEmail('johndoe@ucsd.edu')).toBe(true);
+});
+
+test('Is the email valid: true', () => {
+    expect(functions.isEmail('JOHNDOE@YAHOO.com.vn')).toBe(false);
+});
+
+test('Is the email valid: false', () => {
+    expect(functions.isEmail('johndoe@ucsd@edu')).toBe(false);
+});
+
+// Password Tests
+test('Is the password strong: true', () => {
+    expect(functions.isStrongPassword('a123_456')).toBe(true);
+});
+
+test('Is the password strong: true', () => {
+    expect(functions.isStrongPassword('ThisIsFine1')).toBe(true);
+});
+
+test('Is the password strong: false', () => {
+    expect(functions.isStrongPassword('ABC.DEF')).toBe(false);
+});
+
+test('Is the password strong: false', () => {
+    expect(functions.isStrongPassword('abcdef1234567890')).toBe(false);
+});
+
+// Date Tests
+test('Is the date valid: true', () => {
+    expect(functions.isDate('12/22/1999')).toBe(true);
+});
+
+test('Is the date valid: true', () => {
+    expect(functions.isDate('1/1/1111')).toBe(true);
+});
+
+test('Is the date valid: false', () => {
+    expect(functions.isDate('12-22-1999')).toBe(false);
+});
+
+test('Is the date valid: false', () => {
+    expect(functions.isDate('ab/cd/efgh')).toBe(false);
+});
+
+// Hex Colors Tests
+test('Is this hex code valid: true', () => {
+    expect(functions.isHexColor('#000000')).toBe(true);
+});
+
+test('Is this hex code valid: true', () => {
+    expect(functions.isHexColor('#FFFFFF')).toBe(true);
+});
+
+test('Is this hex code valid: false', () => {
+    expect(functions.isHexColor('#Z12345')).toBe(false);
+});
+
+test('Is this hex code valid: false', () => {
+    expect(functions.isHexColor('@000000')).toBe(false);
+});
